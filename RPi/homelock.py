3f6a733e2f038ed97a40f83c410acedd86d39a58
Wan Leung Wong <wanleungwong@gmail.com>
2018-06-29 18:06:45 +0800

    Add system switch and LED

--- RPi/homelock.py
+++ RPi/homelock.py
@@ -15,12 +15,15 @@ class GracefulKiller:
 
 class HomeLock:
     ### Add param
-    PIN_UNLOCK = 21
-    PIN_LOCK = 20
+    PIN_UNLOCK = 20
+    PIN_LOCK = 21
     PIN_STATE_CLOSE = 13
+    PIN_OFF = 5
+    PIN_DISABLE_LED = 6
     state = 0
     def __init__(self):
         state = 0
+        system_state = 0
 
     def open(self):
         GPIO.setmode(GPIO.BCM)
@@ -28,12 +31,17 @@ class HomeLock:
         GPIO.setup(self.PIN_LOCK, GPIO.OUT)
         GPIO.setup(self.PIN_UNLOCK, GPIO.OUT)
         GPIO.setup(self.PIN_STATE_CLOSE, GPIO.IN)
+        GPIO.setup(self.PIN_OFF, GPIO.IN)
+        GPIO.setup(self.PIN_DISABLE_LED, GPIO.OUT)
 
     def close(self):
         GPIO.cleanup()
 
     def is_door_closed(self):
-        return GPIO.input(self.PIN_STATE_CLOSE) == 1
+        return GPIO.input(self.PIN_STATE_CLOSE) == 0
+
+    def is_system_off(self):
+        return GPIO.input(self.PIN_OFF) == 0
 
     def unlock(self):
         GPIO.output(self.PIN_UNLOCK, GPIO.HIGH)
@@ -44,15 +52,54 @@ class HomeLock:
         GPIO.output(self.PIN_LOCK, GPIO.HIGH)
         time.sleep(1)
         GPIO.output(self.PIN_LOCK, GPIO.LOW)
- 
+
+    def set_system_close(self, isClosed):
+        if isClosed:
+            GPIO.output(self.PIN_DISABLE_LED, GPIO.HIGH)
+            self.system_state = 0 
+        else:
+            GPIO.output(self.PIN_DISABLE_LED, GPIO.LOW)
+            self.system_state = 1
 
 
 def loop(dc):
-    print dc.is_door_closed()
-    if dc.is_door_closed():
-        dc.unlock()
+    ###
+    # door deadbolt
+    # O    C        6
+    # O    O        4
+    # C    C        1
+    # C    O        5
+    # Locking 3  Unlocking 2
+
+    if dc.is_system_off() == False:
+        if dc.system_state == 0:
+            dc.set_system_close(False)
+
+        if dc.state == 0:
+            print dc.state
+        elif dc.state == 1:
+            print dc.state
+        elif dc.state == 2:
+            print dc.state
+        elif dc.state == 3:
+            print dc.state
+        elif dc.state == 4:
+            print dc.state
+        elif dc.state == 5:
+            print dc.state
+        elif dc.state == 6:
+            print dc.state
+        else:
+            print dc.state
+
     else:
-        dc.lock()
+        if dc.system_state == 1:
+            dc.set_system_close(True)
+
+    #if dc.is_door_closed():
+    #    dc.unlock()
+    #else:
+    #    dc.lock()
     time.sleep(1)
 
 
@@ -60,6 +107,16 @@ if __name__ == '__main__':
     killer = GracefulKiller()
     doorchannel = HomeLock()
     doorchannel.open()
+    if doorchannel.is_system_off():
+        doorchannel.state = 0
+        doorchannel.set_system_close(True)
+    else:
+        doorchannel.set_system_close(False)
+        if doorchannel.is_door_closed():
+            doorchannel.lock()
+            doorchannel.state = 1
+            time.sleep(1)
+ 
     while True:
         loop(doorchannel)
         if killer.kill_now:
